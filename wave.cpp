0ca8b1f3f101940a3cb1c3b94f9c4f0c70679fb2
Tomi Adenekan <tominekan12@gmail.com>
2025-07-05 12:30:30 -0400

    Initial commit

---
+++ wave.cpp
@@ -0,0 +1,10 @@
+/**
+ * This is a script for the basic audio manipulations I find myself constantly doing. 
+ * Specifically, setting the audtho
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Lebron james";
+    return 0;
+}
