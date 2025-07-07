f0ea760ed9881cf576cd15f597f16b5c52710f89
Tomi Adenekan <tominekan12@gmail.com>
2025-07-07 13:39:07 -0400

    Added file summarization

--- wave.cpp
+++ wave.cpp
@@ -7,12 +7,14 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <filesystem>
+#include <taglib/fileref.h>
 
 /**
  * Returns an unordered map representing command line arguments.
  * `inputs` is a hash map formatted like so: Keys are the names of the command line flags,
  * and the value of the keys represents the type of the flag associated with the name.
- * There are two types we can specify, `bool` for boolean flags, and `str`, for all other flags.
+ * There are two types we can specify, `bool` for boolean flags, and `str` (really anything not bool), for all other flags.
  * Boolean flags are true if they exist within argv, and `str` flags require a value right after the flag. 
  * 
  * For all values that are non-flags, we return them in a vector keyed to ""  (an empty string).
@@ -21,6 +23,7 @@
  * @param argc the number of command line arguments
  * @param argv the command in list form
  * @param inputs the formatted input
+ * @returns a dictionary representing the command line arguments as specified above
  */
 std::unordered_map<std::string, std::string> parseArguments(int argc, char *argv[], std::unordered_map<std::string, std::string> inputs) {
     std::unordered_map<std::string, std::string> result;
@@ -48,10 +51,15 @@ std::unordered_map<std::string, std::string> parseArguments(int argc, char *argv
                     if (i != (argc - 1)) {
                         std::string flag_value = argv[++i];
                         result.insert({flag_name, flag_value});
+                    } else {
+                        throw std::invalid_argument("");
                     }
                 }
             } catch (const std::out_of_range& e) {
                 std::cerr << "Flag \"" << current << "\" unrecognized\n";
+                exit(-1);
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Flag \"" << current << "\" needs an associated value\n";
                 exit(-1);
             }
         } else {
@@ -69,6 +77,7 @@ std::unordered_map<std::string, std::string> parseArguments(int argc, char *argv
  * 
  * @param text the string we want to split up
  * @param delim the delimiter we are splitting the string by
+ * @returns vector representing the string split up by `delim`
  */
 std::vector<std::string> split_string(std::string text, char delim) {
     std::vector<std::string> result;
@@ -86,19 +95,69 @@ std::vector<std::string> split_string(std::string text, char delim) {
     return result;
 }
 
+/**
+ * Checks if `key` exists in the specified unordered_map.
+ * @param map the unordered_map we want to check if the key exists in
+ * @param key the key that we want to see if exists
+ * @returns true if the key exists, false otherwise
+ */
+bool containsKey(std::unordered_map<std::string, std::string> map, std::string key) {
+    return (map.find(key) != map.end());
+}
+
 int main(int argc, char *argv[]) {
+    // Set our expected arguments
     std::unordered_map<std::string, std::string> expectedArgs = {
-        {"help", "bool"}
+        {"help", "bool"},
+        {"file", "str"},
+        {"set-artist", "str"},
+        {"summarize", "bool"},
+        {"verbose", "bool"},
+        {"version", "bool"}
     };
+
+    // Parse the command line arguments
     std::unordered_map<std::string, std::string> args = parseArguments(argc, argv, expectedArgs);
-    
-    for (const auto& pair : args) {
-        if (pair.first == "") {
-            std::cout << "Passed Inputs (non-flag): " << pair.second << "\n";
-        } else {
-            std::cout << "Flag: " << pair.first << ", Value: " << pair.second << "\n";
-        }
+
+    if (containsKey(args, "help")) {
+        std::cout << "Wave. A command line tool to help with basic audio metadata editing.\n";
+        exit(0);
     }
+
+    if (containsKey(args, "version")) {
+        std::cout << "Wave v0.1beta";
+        exit(0);
+    }
+
+    // It's mandatory this this program exists
+    if (!containsKey(args, "file")) {
+        std::cerr << "program needs input file.\n";
+        exit(-1);
+    }
+    
+    // Ensure that the file exists
+    if (!std::filesystem::is_regular_file(args.at("file"))) {
+        std::cerr << "file \"" << args.at("file") << "\" exists"; 
+        exit(-1);
+    }
+
+    // Now that we can guarantee that the file exists, we can move on
+    try {
+        TagLib::FileRef f(args.at("file").c_str());
+        if (containsKey(args, "summarize")) {
+            std::cout << "Title: " << f.tag()->title() << "\n";
+            std::cout << "Artist: " << f.tag()->artist() << "\n";
+            std::cout << "Author: " << f.tag()->album() << "\n";
+        }
+
+        if (containsKey(args, "set-artist")) {
+            
+        }
+    } catch (...) {
+        std::cerr << "Something went wrong...";
+        exit(1);
+    }
+    
     return 0;
 }
 
