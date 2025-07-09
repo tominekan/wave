d21d73ebf1332bce7d69eee94f3b6d44c81c4f28
Tomi Adenekan <tominekan12@gmail.com>
2025-07-09 11:34:38 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# If any commands fail, the whole thing fails lol
+set -euo pipefail
+INSTALL_PATH=/usr/local/bin
+
+if [-f "wave" ]; then 
+    echo "Moving binary to installation directory"
+    mv wave $INSTALL_PATH
+    echo "Successfully installed wave..."
+else 
+    echo "wave binary doesn't exist, did you run build.sh?"
+fi
+
