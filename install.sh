144984bd4ed8b2baadef664b7b065acee81ad5c4
Tomi Adenekan <tominekan12@gmail.com>
2025-07-11 11:05:21 -0400

    Fixed install.sh to work properly

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 # If any commands fail, the whole thing fails lol
 set -euo pipefail
+INSTALL_PATH="/usr/local/bin"
 
 # Build wave, we're assuming taglib is installed 
 g++ -std=c++17 wave.cpp -o wave -ltag -lz
 
 # Install taglib to a specified directory
-if [-f "wave" ]; then 
+if [ -f "wave" ]; then 
     echo "Moving binary to installation directory"
     mv wave $INSTALL_PATH
     echo "Successfully installed wave..."
