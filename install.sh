4eff402112b1cd7989217131d5ff86e522f7b501
Tomi Adenekan <tominekan12@gmail.com>
2025-07-11 11:03:06 -0400

    Lol install.sh isn't working, fixing to automatically check if taglib is installed

--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@
 
 # If any commands fail, the whole thing fails lol
 set -euo pipefail
-INSTALL_PATH=/usr/local/bin
 
-# -ltag includes the taglib library, and lz includes the zlib library
-if ! taglib-config --version then
-    echo "Couldn't find taglib in your system, do you have it installed?"
-    exit 1
-fi
-
-# Build taglib
+# Build wave, we're assuming taglib is installed 
 g++ -std=c++17 wave.cpp -o wave -ltag -lz
 
 # Install taglib to a specified directory
